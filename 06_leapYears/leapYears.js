7601fa2ef2a36b546822e2668b0c1da9224b653a
Teo Hao Yu <thstoneyes@gmail.com>
2022-07-04 18:43:31 +0800

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0 && year % 100 != 0) return true;
+  return year % 400 == 0;
 };
 
 // Do not edit below this line
