d1d92ebe8bbf5b775e54280aaa15288d0e2f6681
Teo Hao Yu <thstoneyes@gmail.com>
2022-07-04 18:26:34 +0800

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  if (!isValid(first, second)) return "ERROR";
+  let result = 0;
+  let bigger = first > second ? first : second;
+  let smaller = first < second ? first : second;
+  for (let i = smaller; i <= bigger; i++) {
+    result += i;
+  }
+  return result;
 };
+
+function isValid(first, second) {
+  if (typeof first !== "number" || typeof second !== "number") {
+    return false;
+  } else if (first < 0 || second < 0) {
+    return false;
+  }
+
+  return true;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
