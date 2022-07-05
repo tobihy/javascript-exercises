9732e48e04dc024da996ba9fcc6e5301bdecacb9
Teo Hao Yu <thstoneyes@gmail.com>
2022-07-05 14:52:19 +0800

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (tempF) {
+  let result = (tempF - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempC) {
+  let result = tempC * (9 / 5) + 32;
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
